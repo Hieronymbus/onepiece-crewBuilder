d538651a2384342ff32f0564cbb6c46188f9f7fa
Hieronymbus <birdsallalexander1990@gmail.comm>
2024-10-02 15:01:13 +1300

    modal open button done pirate page

--- frontend/src/pages/PiratesPage.jsx
+++ frontend/src/pages/PiratesPage.jsx
@@ -5,15 +5,28 @@ const PiratesPage = () => {
   
   const [isModalOpen, setIsModalOpen] = useState(false)
   
+  const handleOpenModal = ( ) => {
+      setIsModalOpen(true)
+  }
+
   return (
-    <div className='h-5/6 flex flex-col  items-center'>
-      <h1 className='text-5xl'>Known Pirates</h1>
-      <div className='w-11/12 h-5/6 border-2 border-orange-900 p-4'>
+    <div className='h-5/6 flex flex-col items-center'>
+      <div className='m-3 w-full '>
+        <h1 className='text-5xl'>Known Pirates</h1>
+        <div className='w-full flex justify-end'>
+          <button 
+            className='h-1/2 w-1/5 text-4xl flex justify-center items-center text-amber-950 bg-wood-brown dark:bg-wood-brown-dark rounded-lg shadow-lg'
+            onClick={handleOpenModal}
+          >
+          Create New Pirate
+          </button>
+        </div>
         
       </div>
-      {isModalOpen && <PirateFormModal />}
-    
-    
+      <div className='w-11/12 h-5/6 border-2 border-orange-900 p-4'> 
+      </div>
+
+      {isModalOpen && <PirateFormModal setIsModalOpen={setIsModalOpen} />}
     </div>
 
   )
